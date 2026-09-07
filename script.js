beb8056d3246759a6a5e91f3634dfc0c455541dc
ctech1721-ops <ctech1721@gmail.com>
2026-09-08 00:19:37 +0530

    Connect frontend to live backend

--- script.js
+++ script.js
@@ -37,7 +37,7 @@
    CONFIG
    ========================================================= */
 
-const API_BASE = "https://sri-kumar-stores-hovt-64kf3ef54-mohanam.vercel.app";
+const API_BASE = "https://sri-kumar-stores-hovt-easl0cjt1-mohanam.vercel.app";
 
 const PRODUCT_KEY = "textel_products";
 const CART_KEY = "textel_cart";
