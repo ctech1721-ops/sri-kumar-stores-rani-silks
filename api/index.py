3fe747d5c67d8dcadbca0c6cef55a66a6255e9cb
ctech1721-ops <ctech1721@gmail.com>
2026-09-07 23:16:25 +0530

    Fix api index

--- api/index.py
+++ api/index.py
@@ -1,1 +1,9 @@
-from backend.app import app
+import sys
+import os
+
+backend_path = os.path.join(os.path.dirname(__file__), "backend")
+
+if backend_path not in sys.path:
+    sys.path.insert(0, backend_path)
+
+from app import app
